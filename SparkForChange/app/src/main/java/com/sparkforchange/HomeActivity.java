dd1ab459fc55bfd223c2140da8bf881f10689b0d
COREY FOGG <cfogg6@gatech.edu>
2018-01-27 15:51:12 -0500

    started updating homeactivity

--- SparkForChange/app/src/main/java/com/sparkforchange/HomeActivity.java
+++ SparkForChange/app/src/main/java/com/sparkforchange/HomeActivity.java
@@ -2,12 +2,18 @@ package com.sparkforchange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button donateBtn, volunteerBtn, redeemBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        donateBtn = findViewById(R.id.btn_donate);
+        volunteerBtn = findViewById(R.id.btn_volunteer);
+        redeemBtn = findViewById(R.id.btn_redeem);
     }
 }
