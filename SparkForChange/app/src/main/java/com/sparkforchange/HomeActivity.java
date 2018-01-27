0554f552c64f717d647dd5e5a44e291ddfcea288
COREY FOGG <cfogg6@gatech.edu>
2018-01-27 16:00:11 -0500

    created controller directory

--- SparkForChange/app/src/main/java/com/sparkforchange/HomeActivity.java
+++ SparkForChange/app/src/main/java/com/sparkforchange/HomeActivity.java
@@ -2,11 +2,14 @@ package com.sparkforchange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button donateBtn, volunteerBtn, redeemBtn;
+    Button donateBtn, volunteerBtn, redeemBtn, profileBtn, settingsBtn;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,15 @@ public class HomeActivity extends AppCompatActivity {
         donateBtn = findViewById(R.id.btn_donate);
         volunteerBtn = findViewById(R.id.btn_volunteer);
         redeemBtn = findViewById(R.id.btn_redeem);
+        profileBtn = findViewById(R.id.btn_profile);
+        settingsBtn = findViewById(R.id.btn_settings);
+        searchView = findViewById(R.id.sv_search);
+
+        donateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
