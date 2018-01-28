6d2acfecfcdbea6f0d82405c215dc09867d18f76
Cole Bowers <cole.a.bowers@gmail.com>
2018-01-27 21:51:32 -0500

    payment information controller

--- SparkForChange/app/src/main/java/com/sparkforchange/controllers/PaymentMethodActivity.java
+++ SparkForChange/app/src/main/java/com/sparkforchange/controllers/PaymentMethodActivity.java
@@ -13,28 +13,45 @@ import com.sparkforchange.model.User;
 
 public class PaymentMethodActivity extends AppCompatActivity {
     Button updateBtn;
+    EditText addressEt;
+    EditText creditCardEt;
+    EditText cvvEt;
+    EditText expDateEt;
+    EditText paymentNameEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_method);
+
+        addressEt = findViewById(R.id.et_address);
+        creditCardEt = findViewById(R.id.et_creditcard);
+        cvvEt = findViewById(R.id.et_cvv);
+        expDateEt = findViewById(R.id.et_expiration_date);
+        paymentNameEt = findViewById(R.id.et_name);
+        User curUser = Facade.getInstance().getCurrentUser();
+        if(curUser.getAddress() != null)
+            addressEt.setText(curUser.getAddress());
+        if(curUser.getCreditCard() != null)
+            creditCardEt.setText(curUser.getCreditCard());
+        if(curUser.getCvv() != null)
+            cvvEt.setText(curUser.getCvv());
+        if(curUser.getExpDate() != null)
+            expDateEt.setText(curUser.getExpDate());
+        if(curUser.getPaymentName() != null)
+            paymentNameEt.setText(curUser.getPaymentName());
+
         updateBtn = findViewById(R.id.btn_update);
         updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 User curUser = Facade.getInstance().getCurrentUser();
-                EditText et = findViewById(R.id.et_address);
-                curUser.setAddress(et.getText().toString());
-                et = findViewById(R.id.et_creditcard);
-                curUser.setCreditCard(et.getText().toString());
-                et = findViewById(R.id.et_cvv);
-                curUser.setCvv(et.getText().toString());
-                et = findViewById(R.id.et_expiration_date);
-                curUser.setExpDate(et.getText().toString());
-                et = findViewById(R.id.et_name);
-                curUser.setPaymentName(et.getText().toString());
-
+                curUser.setAddress(addressEt.getText().toString());
+                curUser.setCreditCard(creditCardEt.getText().toString());
+                curUser.setCvv(cvvEt.getText().toString());
+                curUser.setExpDate(expDateEt.getText().toString());
+                curUser.setPaymentName(paymentNameEt.getText().toString());
                 Toast.makeText(getApplicationContext(), "$$$ Payment Method Updated $$$",
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_LONG).show();
                 finish();
             }
         });
