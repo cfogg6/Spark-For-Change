f0c1e7489a867a8822318bfeb2377c20c71e76a0
nancytao <nancy.tao42@gmail.com>
2018-01-27 21:51:28 -0500

    Added timeline button functionality

--- SparkForChange/app/src/main/java/com/sparkforchange/controllers/ProfileActivity.java
+++ SparkForChange/app/src/main/java/com/sparkforchange/controllers/ProfileActivity.java
@@ -1,11 +1,8 @@
 package com.sparkforchange.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Button;
 
 import com.sparkforchange.R;
 
@@ -15,6 +12,11 @@ public class ProfileActivity extends ToolbarDrawerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-    }
 
+        Button volunteerHistoryBtn = findViewById(R.id.btn_timeline);
+
+        volunteerHistoryBtn.setOnClickListener(view -> {
+            startActivity(new Intent(ProfileActivity.this, TimelineActivity.class));
+        });
+    }
 }
